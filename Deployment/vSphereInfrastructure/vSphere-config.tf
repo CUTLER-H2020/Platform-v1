f3a124f331836dcdece9a6eb93011e9998739461
APOB83 <andrewbyrne@gmail.com>
2019-08-14 11:37:33 +0000

    Updated VM configurations

--- Deployment/vSphereInfrastructure/vSphere-config.tf
+++ Deployment/vSphereInfrastructure/vSphere-config.tf
@@ -26,53 +26,190 @@ data "vsphere_network" "network" {
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
-data "vsphere_virtual_machine" "template" {
-  name          = "${var.vm_template}"
+data "vsphere_virtual_machine" "c1_template" {
+  name          = "${var.c1_template}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+data "vsphere_virtual_machine" "c2_template" {
+  name          = "${var.c2_template}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+data "vsphere_virtual_machine" "c3_template" {
+  name          = "${var.c3_template}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+data "vsphere_virtual_machine" "c4_template" {
+  name          = "${var.c4_template}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
-resource "vsphere_virtual_machine" "vm" {
-  count 			= "${var.num_of_vms}"
-  name             	= "${var.vm_name_prefix}${count.index}"
+data "vsphere_host" "host" {
+  name          = "${var.vsphere_host}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+resource "vsphere_virtual_machine" "c1_vms" {
+  count 		= "${var.num_of_c1}"
+  name             	= "${var.c1_name_prefix}${count.index}"
   resource_pool_id 	= "${data.vsphere_resource_pool.pool.id}"
   datastore_id     	= "${data.vsphere_datastore.datastore.id}"
+  host_system_id	= "${data.vsphere_host.host.id}"
 
-  num_cpus = 4
-  memory   = 8192
-  guest_id 	= "${data.vsphere_virtual_machine.template.guest_id}"
+  num_cpus 	= "${var.c1_cpu}"
+  memory   	= "${var.c1_ram}"
+  guest_id 	= "${data.vsphere_virtual_machine.c1_template.guest_id}"
+  scsi_type 	= "${data.vsphere_virtual_machine.c1_template.scsi_type}"
   
-  scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
-
   network_interface {
     network_id   	= "${data.vsphere_network.network.id}"
     adapter_type 	= "vmxnet3"
   }
 
   disk {
-    label 				= "disk0"
-    size				= "${var.disk_size}"
-    thin_provisioned 	= "true"
+    label 		= "disk0"
+    size		= "${var.c1_disk_size}"
+    thin_provisioned 	= "false"
   }
   
   clone {
-    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+    template_uuid = "${data.vsphere_virtual_machine.c1_template.id}"
 
     customize {
       linux_options {
         
-		host_name = "${format("%s%03d", var.vm_name_prefix, count.index + 1)}"
+		host_name = "${format("%s%03d", var.c1_name_prefix, count.index + 1)}"
 		domain    = "$(var.vm_domain_name)"
       }
-
       network_interface {
         ipv4_address    = "${cidrhost(var.vm_network_address, var.vm_ip_address_start + count.index)}"
-        ipv4_netmask    = 24
+        ipv4_netmask    = 22
       }
 	  ipv4_gateway = "${var.vm_gateway}"
-	  dns_server_list = ["${split(",", var.vm_dns_servers)}"]
-      
-    }
-		
+	  dns_server_list = ["8.8.8.8","1.1.1.1"] 
+    }		
   }
-  
 }
+
+resource "vsphere_virtual_machine" "c2_vms" {
+  count                 = "${var.num_of_c2}"
+  name                  = "${var.c2_name_prefix}${count.index}"
+  resource_pool_id      = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id          = "${data.vsphere_datastore.datastore.id}"
+  host_system_id        = "${data.vsphere_host.host.id}"
+
+  num_cpus      = "${var.c2_cpu}"
+  memory        = "${var.c2_ram}"
+  guest_id      = "${data.vsphere_virtual_machine.c2_template.guest_id}"
+  scsi_type     = "${data.vsphere_virtual_machine.c2_template.scsi_type}"
+
+  network_interface {
+    network_id          = "${data.vsphere_network.network.id}"
+    adapter_type        = "vmxnet3"
+  }
+
+  disk {
+    label               = "disk0"
+    size                = "${var.c2_disk_size}"
+    thin_provisioned    = "false"
+  }
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.c2_template.id}"
+
+    customize {
+      linux_options {
+
+                host_name = "${format("%s%03d", var.c2_name_prefix, count.index + 1)}"
+                domain    = "$(var.vm_domain_name)"
+      }
+      network_interface {
+        ipv4_address    = "${cidrhost(var.vm_network_address, var.vm_ip_address_start + count.index + var.num_of_c1)}"
+        ipv4_netmask    = 22
+      }
+          ipv4_gateway = "${var.vm_gateway}"
+          dns_server_list = ["8.8.8.8","1.1.1.1"]
+    }
+  }
+}
+
+resource "vsphere_virtual_machine" "c3_vms" {
+  count                 = "${var.num_of_c3}"
+  name                  = "${var.c3_name_prefix}${count.index}"
+  resource_pool_id      = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id          = "${data.vsphere_datastore.datastore.id}"
+  host_system_id        = "${data.vsphere_host.host.id}"
+
+  num_cpus      = "${var.c3_cpu}"
+  memory        = "${var.c3_ram}"
+  guest_id      = "${data.vsphere_virtual_machine.c3_template.guest_id}"
+  scsi_type     = "${data.vsphere_virtual_machine.c3_template.scsi_type}"
+
+  network_interface {
+    network_id          = "${data.vsphere_network.network.id}"
+    adapter_type        = "vmxnet3"
+  }
+
+  disk {
+    label               = "disk0"
+    size                = "${var.c3_disk_size}"
+    thin_provisioned    = "false"
+  }
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.c3_template.id}"
+
+    customize {
+      linux_options {
+
+                host_name = "${format("%s%03d", var.c3_name_prefix, count.index + 1)}"
+                domain    = "$(var.vm_domain_name)"
+      }
+      network_interface {
+        ipv4_address    = "${cidrhost(var.vm_network_address, var.vm_ip_address_start + count.index + var.num_of_c1 + var.num_of_c2)}"
+        ipv4_netmask    = 22
+      }
+          ipv4_gateway = "${var.vm_gateway}"
+          dns_server_list = ["8.8.8.8","1.1.1.1"]
+    }
+  }
+}
+
+resource "vsphere_virtual_machine" "c4_vms" {
+  count                 = "${var.num_of_c4}"
+  name                  = "${var.c4_name_prefix}${count.index}"
+  resource_pool_id      = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id          = "${data.vsphere_datastore.datastore.id}"
+  host_system_id        = "${data.vsphere_host.host.id}"
+
+  num_cpus      = "${var.c4_cpu}"
+  memory        = "${var.c4_ram}"
+  guest_id      = "${data.vsphere_virtual_machine.c2_template.guest_id}"
+  scsi_type     = "${data.vsphere_virtual_machine.c2_template.scsi_type}"
+
+  network_interface {
+    network_id          = "${data.vsphere_network.network.id}"
+    adapter_type        = "vmxnet3"
+  }
+
+  disk {
+    label               = "disk0"
+    size                = "${var.c4_disk_size}"
+    thin_provisioned    = "false"
+  }
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.c4_template.id}"
+
+    customize {
+      linux_options {
+
+                host_name = "${format("%s%03d", var.c4_name_prefix, count.index + 1)}"
+                domain    = "$(var.vm_domain_name)"
+      }
+      network_interface {
+        ipv4_address    = "${cidrhost(var.vm_network_address, var.vm_ip_address_start + count.index + var.num_of_c1 + var.num_of_c2 + var.num_of_c3)}"
+        ipv4_netmask    = 22
+      }
+          ipv4_gateway = "${var.vm_gateway}"
+          dns_server_list = ["8.8.8.8","1.1.1.1"]
+    }
+  }
+}
+
--- Deployment/vSphereInfrastructure/variables.tf
+++ Deployment/vSphereInfrastructure/variables.tf
@@ -5,24 +5,59 @@ variable "vsphere_server" {}
 variable "vsphere_dc" {}
 variable "vsphere_ds1" {}
 variable "vsphere_rp" {}
+variable "vsphere_host" {}
 
 variable "num_of_vms" {
   type = "string"
 }
+variable "num_of_c1" {
+  type = "string"
+}
+variable "num_of_c2" {
+  type = "string"
+}
+variable "num_of_c3" {
+  type = "string"
+}
+variable "num_of_c4" {
+  type = "string"
+}
+
 
 variable "vm_name_prefix" {
   type = "string"
 }
-
 variable "vm_template" {
+  type = "string"
+}
+variable "c1_name_prefix" {
+  type = "string"
+}
+variable "c1_template" {
+  type = "string"
+}
+variable "c2_name_prefix" {
+  type = "string"
+}
+variable "c2_template" {
+  type = "string"
+}
+variable "c3_name_prefix" {
+  type = "string"
+}
+variable "c3_template" {
+  type = "string"
+}
+variable "c4_name_prefix" {
+  type = "string"
+}
+variable "c4_template" {
   type = "string"
 }
 
 variable "vm_network" {
   type = "string"
 }
-
-
 variable "vm_network_address" {
   type = "string"
 }
@@ -30,20 +65,66 @@ variable "vm_network_address" {
 variable "vm_ip_address_start" {
   default = "<INTEGER>"
 }
+variable "c1_ip_address_start" {
+  default = "<INTEGER>"
+}
+variable "c2_ip_address_start" {
+  default = "<INTEGER>"
+}
+variable "c3_ip_address_start" {
+  default = "<INTEGER>"
+}
+variable "c4_ip_address_start" {
+  default = "<INTEGER>"
+}
 
 variable "vm_gateway" {
   type = "string"
 }
-
-variable "disk_size" {
-  type = "string"
-}
-
 variable "vm_dns_servers" {
   type = "string"
 }
-
 variable "vm_domain_name" {
   description = "The domain of the virtual machine"
   default     = "<STRING>"
 }
+
+variable "c1_cpu" {
+  type = "string"
+}
+variable "c2_cpu" {
+  type = "string"
+}
+variable "c3_cpu" {
+  type = "string"
+}
+variable "c4_cpu" {
+  type = "string"
+}
+
+variable "c1_ram" {
+  type = "string"
+}
+variable "c2_ram" {
+  type = "string"
+}
+variable "c3_ram" {
+  type = "string"
+}
+variable "c4_ram" {
+  type = "string"
+}
+
+variable "c1_disk_size" {
+  type = "string"
+}
+variable "c2_disk_size" {
+  type = "string"
+}
+variable "c3_disk_size" {
+  type = "string"
+}
+variable "c4_disk_size" {
+  type = "string"
+}
+
